9c64c2bfc86df1bee9ffd74520454391ab672749
George <wallzy@mail.ru>
2025-07-28 20:12:49 +0300

    open graph

--- client/src/components/Share.tsx
+++ client/src/components/Share.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import './Share.css'
 import useClickOutside from '../hooks/useClickOutside'
 
