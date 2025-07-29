92c9b3dfa7a3ae77325575782493ace1d60d7c95
George <wallzy@mail.ru>
2025-07-29 22:42:56 +0300

    preloader style

--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -1,3 +1,4 @@
+import Preloader from '../components/Preloader';
 import InteractiveMap from '../components/InteractiveMap';
 import InfoPanel from '../components/InfoPanel';
 import InfoPanelVertical from '../components/InfoPanelVertical';
@@ -5,7 +6,6 @@ import './MapPage.css';
 import Share from '../components/Share';
 import Filter from '../components/Filter';
 import { usePreloadStore } from '../store/preloadStore';
-import Preloader from '../components/Preloader';
 import { useEffect, useRef, useState } from 'react';
 
 export default function MapPage() {
@@ -13,7 +13,7 @@ export default function MapPage() {
   const [endCountdown, setEndCountdown] = useState(false);
   const [preloaderVisible, setPreloaderVisible] = useState(true);
   const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-  
+
   // Ставим таймер на 2 секунды один раз
   useEffect(() => {
     timerRef.current = setTimeout(() => {
